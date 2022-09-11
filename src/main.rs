ac9e15f0bb13b6db66e1a56b0f14cbac8a260b39
Scott Donnelly <scott@donnel.ly>
2022-09-11 08:35:47 +0100

    test: add benches similar to those of FNNTW

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use num_traits::Float;
-use rand::Rng;
-use sok::distance::squared_euclidean;
 use sok::KdTree;
 
 fn main() {
-    let mut tree: KdTree<f64, i32, 2, 4> = KdTree::new();
+    let _tree: KdTree<f64, i32, 2, 4> = KdTree::new();
 
 
 }
