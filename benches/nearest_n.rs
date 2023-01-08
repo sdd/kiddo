68b74b7b0176813178c716055de9b582153c6ee6
Scott Donnelly <scott@donnel.ly>
2023-01-08 23:10:23 +0000

    test: update benches

--- benches/nearest_n.rs
+++ benches/nearest_n.rs
@@ -1,24 +1,71 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use az::Cast;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput, PlotConfiguration, AxisScale, BenchmarkGroup, BatchSize};
+use criterion::measurement::WallTime;
+use rand::distributions::Standard;
 // use rayon::prelude::*;
 
 use rand_distr::Distribution;
 use rand_distr::UnitSphere as SPHERE;
 use sok::float::distance::squared_euclidean;
-use sok::float::kdtree::KdTree;
+use sok::float::kdtree::{Axis, Content, Index, KdTree};
 
 const K: usize = 3;
 const BUCKET_SIZE: usize = 32;
 const QUERY: usize = 1_000;
 
-fn rand_unit_sphere_point_f32() -> [f32; 3] {
+/*fn rand_unit_sphere_point_f32() -> [f32; 3] {
     SPHERE.sample(&mut rand::thread_rng())
 }
 
 fn rand_sphere_data() -> ([f32; 3], usize) {
     (rand_unit_sphere_point_f32(), rand::random())
+}*/
+
+pub fn nearest_n(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Query Nearest n");
+
+    let plot_config = PlotConfiguration::default()
+        .summary_scale(AxisScale::Logarithmic);
+
+    group.plot_config(plot_config);
+
+    for &size in [100, 1_000, 10_000, 100_000].iter() {
+        bench_query_nearest_n_float::<f64, u32, 2, u16>(&mut group, size, 100,"2D f64");
+    }
 }
 
-pub fn nearest_n_10_euclidean2(c: &mut Criterion) {
+fn bench_query_nearest_n_float<A: Axis, T: Content, const K: usize, IDX: Index<T = IDX>>(group: &mut BenchmarkGroup<WallTime>, initial_size: usize, nearest_qty: usize, subtype: &str) where usize: Cast<IDX>, Standard: Distribution<([A; K], T)> {
+    group.bench_with_input(BenchmarkId::new(subtype, initial_size), &initial_size, |b, &size| {
+        let mut res: Vec<(A, T)> = Vec::with_capacity(nearest_qty);
+        b.iter_batched(|| {
+            let points_to_query: Vec<[A; K]> =
+                (0..qty_to_add).into_iter().map(|_| rand::random::<[A; K]>()).collect();
+
+            let mut initial_points = vec![];
+            for _ in 0..size {
+                initial_points.push(rand::random::<([A; K], T)>());
+            }
+            let mut kdtree =
+                KdTree::<A, T, K, BUCKET_SIZE, IDX>::with_capacity(size);
+
+            for i in 0..initial_points.len() {
+                kdtree.add(&initial_points[i].0, initial_points[i].1);
+            }
+
+            res.clear();
+
+            (kdtree, points_to_query)
+        }, |(mut kdtree, points_to_query)| {
+            black_box(points_to_query
+                .iter()
+                .for_each(|point| {
+                    res.extend(kdtree.nearest_n(&point, nearest_qty, &squared_euclidean));
+                }))
+        }, BatchSize::SmallInput);
+    });
+}
+
+/*pub fn nearest_n_10_euclidean2(c: &mut Criterion) {
     let mut group = c.benchmark_group("nearest_n(10)");
 
     for size in [1_000, 10_000, 100_000, 1_000_000].iter() {
@@ -44,7 +91,7 @@ pub fn nearest_n_10_euclidean2(c: &mut Criterion) {
             }));
         });
     }
-}
+}*/
 
-criterion_group!(benches, nearest_n_10_euclidean2);
+criterion_group!(benches, nearest_n);
 criterion_main!(benches);
--- benches/within_unsorted.rs
+++ benches/within_unsorted.rs
@@ -1,9 +1,11 @@
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use az::Cast;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput, BenchmarkGroup};
+use criterion::measurement::WallTime;
 
 use rand_distr::Distribution;
 use rand_distr::UnitSphere as SPHERE;
 use sok::distance::squared_euclidean;
-use sok::KdTree;
+use sok::float::kdtree::{Index, KdTree};
 
 const BUCKET_SIZE: usize = 32;
 
@@ -15,69 +17,56 @@ fn rand_sphere_data() -> ([f64; 3], usize) {
     (rand_unit_sphere_point_f64(), rand::random())
 }
 
+fn bench_within_unsorted_3d<IDX: Index<T = IDX>>(group: &mut BenchmarkGroup<WallTime>, size: &usize, radius: f64) where usize: Cast<IDX>  {
+    group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
+        let point = rand_sphere_data();
+
+        let mut points = vec![];
+        let mut kdtree = KdTree::<_, _, 3, BUCKET_SIZE, IDX>::with_capacity(size);
+        for _ in 0..size {
+            points.push(rand_sphere_data());
+        }
+        for i in 0..points.len() {
+            kdtree.add(&points[i].0, points[i].1);
+        }
+
+        b.iter(|| black_box(kdtree.within_unsorted(&point.0, radius, &squared_euclidean)));
+    });
+}
+
 pub fn within_unsorted_small_euclidean2(c: &mut Criterion) {
     let mut group = c.benchmark_group("within_unsorted(0.01)");
+    group.throughput(Throughput::Elements(1));
 
     for size in [100, 1_000, 10_000, 100_000, 1_000_000].iter() {
-        group.throughput(Throughput::Elements(1));
-        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            let point = rand_sphere_data();
-
-            let mut points = vec![];
-            let mut kdtree = KdTree::<_, _, 3, BUCKET_SIZE>::with_capacity(size);
-            for _ in 0..size {
-                points.push(rand_sphere_data());
-            }
-            for i in 0..points.len() {
-                kdtree.add(&points[i].0, points[i].1);
-            }
-
-            b.iter(|| black_box(kdtree.within_unsorted(&point.0, 0.01, &squared_euclidean)));
-        });
+        bench_within_unsorted_3d::<u16>(&mut group, size, 0.01);
+    }
+    for size in [1_000_000].iter() {
+        bench_within_unsorted_3d::<u32>(&mut group, size, 0.01);
     }
 }
 
 pub fn within_unsorted_medium_euclidean2(c: &mut Criterion) {
     let mut group = c.benchmark_group("within_unsorted(0.05)");
+    group.throughput(Throughput::Elements(1));
 
     for size in [100, 1_000, 10_000, 100_000, 1_000_000].iter() {
-        group.throughput(Throughput::Elements(1));
-        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            let point = rand_sphere_data();
-
-            let mut points = vec![];
-            let mut kdtree = KdTree::<_, _, 3, BUCKET_SIZE>::with_capacity(size);
-            for _ in 0..size {
-                points.push(rand_sphere_data());
-            }
-            for i in 0..points.len() {
-                kdtree.add(&points[i].0, points[i].1);
-            }
-
-            b.iter(|| black_box(kdtree.within_unsorted(&point.0, 0.05, &squared_euclidean)));
-        });
+        bench_within_unsorted_3d::<u16>(&mut group, size, 0.05);
+    }
+    for size in [1_000_000].iter() {
+        bench_within_unsorted_3d::<u32>(&mut group, size, 0.05);
     }
 }
 
 pub fn within_unsorted_large_euclidean2(c: &mut Criterion) {
     let mut group = c.benchmark_group("within_unsorted(0.25)");
+    group.throughput(Throughput::Elements(1));
 
     for size in [100, 1_000, 10_000, 100_000, 1_000_000].iter() {
-        group.throughput(Throughput::Elements(1));
-        group.bench_with_input(BenchmarkId::from_parameter(size), size, |b, &size| {
-            let point = rand_sphere_data();
-
-            let mut points = vec![];
-            let mut kdtree = KdTree::<_, _, 3, BUCKET_SIZE>::with_capacity(size);
-            for _ in 0..size {
-                points.push(rand_sphere_data());
-            }
-            for i in 0..points.len() {
-                kdtree.add(&points[i].0, points[i].1);
-            }
-
-            b.iter(|| black_box(kdtree.within_unsorted(&point.0, 0.25, &squared_euclidean)));
-        });
+        bench_within_unsorted_3d::<u16>(&mut group, size, 0.25);
+    }
+    for size in [1_000_000].iter() {
+        bench_within_unsorted_3d::<u32>(&mut group, size, 0.25);
     }
 }
 
