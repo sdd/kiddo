354c3fd3ad1a0df4aba1afb11ddb1bd3296d3277
Scott Donnelly <scott@donnel.ly>
2023-05-27 01:32:55 +0100

    docs: update changelog and docs for release 2.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 #![warn(missing_docs)]
 #![warn(rustdoc::broken_intra_doc_links)]
 #![warn(rustdoc::private_intra_doc_links)]
-#![doc(html_root_url = "https://docs.rs/kiddo/2.0.2")]
+#![doc(html_root_url = "https://docs.rs/kiddo/2.1.0")]
 #![doc(issue_tracker_base_url = "https://github.com/sdd/kiddo/issues/")]
 
 //! # Kiddo
@@ -29,7 +29,7 @@
 //! Add `kiddo` to `Cargo.toml`
 //! ```toml
 //! [dependencies]
-//! kiddo = "2.0.2"
+//! kiddo = "2.1.0"
 //! ```
 //!
 //! ## Usage
