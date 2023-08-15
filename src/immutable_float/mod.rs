115bfdc5da8e55127bd83e166a583c3c51654d66
Scott Donnelly <scott@donnel.ly>
2023-08-15 21:22:56 +0100

    chore: minor example and docs update for immutable

--- src/immutable_float/mod.rs
+++ src/immutable_float/mod.rs
@@ -1,12 +1,28 @@
 //! Immutable floating point k-d tree.
-//! Intended for use when the co-ordinates of the points being stored in the tree
-//! are floats ([`f64`] or [`f32`] are supported currently),
-//! the contents have already been generated in advance,
-//! and items will not need to be added or removed from the
-//! tree once it has been constructed.
-//! These constraints permit improvements to be made in both
-//! space and query time performance, at the cost of construction
-//! time performance.
+//!
+//! Intended for use when the smallest possible on-disk serialized
+//! size of a tree is of paramount importance, and / or the
+//! fastest possible query speed is required.
+//!
+//! Expect improvements in query time of 10-15%, and a reduction
+//! in the size of serialized trees by 33% or so on average.  
+//!
+//! These capabilities come with a few trade-offs:
+//! 1) This tree does not provide the capability
+//!    to modify its contents after it has been constructed.
+//!    The co-ordinates of the points to be stored must
+//!    have all been generated in advance.
+//! 2) Construction time can be quite a bit slower. Typically
+//!    this can be twice as long as the default `kiddo::float::kdtree::KdTree`.
+//! 3) The more common that duplicate values are amongst your source points,
+//!    the slower it will take to construct the tree. If you're using `f64`
+//!    data that is fairly random-ish, you will probably not encounter any issues.
+//!    I've successfully created 250 million node `ImmutableTree` instances with random
+//!    [`f64`] data with no issues, limited only by RAM during construction.
+//!    Likewise for [`f32`] based trees, up to a few million nodes.
+//!
+//! As per the other Kiddo float-type trees, points being stored
+//! in the tree must be floats ([`f64`] or [`f32`] are supported currently).
 
 #[doc(hidden)]
 pub mod construction;
