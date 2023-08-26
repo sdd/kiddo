60eb3e04fa87920ab196d3fa68643ca70c8b71a4
Scott Donnelly <scott@donnel.ly>
2023-08-26 20:05:04 +0100

    feat: refactor immutable to macros to allow DRY query method generation for ArchivedImmutableKdTree

---
+++ src/immutable/common/generate_within.rs
@@ -0,0 +1,17 @@
+#[macro_export]
+macro_rules! generate_immutable_within {
+    ($comments:tt) => {
+        doc_comment! {
+            concat!$comments,
+            #[inline]
+            pub fn within<D>(&self, query: &[A; K], dist: A) -> Vec<NearestNeighbour<A, T>>
+            where
+                D: DistanceMetric<A, K>,
+            {
+                let mut matching_items = self.within_unsorted::<D>(query, dist);
+                matching_items.sort();
+                matching_items
+            }
+        }
+    };
+}
