079d47ab9efa74c579d1919f0a05cae5d84640be
Scott Donnelly <scott@donnel.ly>
2023-08-28 19:32:03 +0100

    chore: remove commented code and test comments

--- src/immutable/float/query/within_unsorted.rs
+++ src/immutable/float/query/within_unsorted.rs
@@ -63,132 +63,6 @@ impl<
     );
 }
 
-/*impl<A: Axis, T: Content, const K: usize, const B: usize> ImmutableKdTree<A, T, K, B> {
-    /// Finds all elements within `dist` of `query`, using the specified
-    /// distance metric function.
-    ///
-    /// Results are returned in arbitrary order. Faster than `within`.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use kiddo::immutable::float::kdtree::ImmutableKdTree;
-    /// use kiddo::float::distance::SquaredEuclidean;
-    ///
-    /// let content: Vec<[f64; 3]> = vec!(
-    ///     [1.0, 2.0, 5.0],
-    ///     [2.0, 3.0, 6.0],
-    ///     [200.0, 300.0, 600.0],
-    /// );
-    ///
-    /// let mut tree: ImmutableKdTree<f64, u32, 3, 32> = ImmutableKdTree::new_from_slice(&content);
-    ///
-    /// let within = tree.within_unsorted::<SquaredEuclidean>(&[1.0, 2.0, 5.0], 10f64);
-    ///
-    /// assert_eq!(within.len(), 2);
-    /// ```
-    #[inline]
-    pub fn within_unsorted<D>(&self, query: &[A; K], dist: A) -> Vec<NearestNeighbour<A, T>>
-    where
-        D: DistanceMetric<A, K>,
-    {
-        let mut off = [A::zero(); K];
-        let mut matching_items = Vec::new();
-
-        self.within_unsorted_recurse::<D>(
-            query,
-            dist,
-            1,
-            0,
-            &mut matching_items,
-            &mut off,
-            A::zero(),
-        );
-
-        matching_items
-    }
-
-    #[allow(clippy::too_many_arguments)]
-    fn within_unsorted_recurse<D>(
-        &self,
-        query: &[A; K],
-        radius: A,
-        stem_idx: usize,
-        split_dim: usize,
-        matching_items: &mut Vec<NearestNeighbour<A, T>>,
-        off: &mut [A; K],
-        rd: A,
-    ) where
-        D: DistanceMetric<A, K>,
-    {
-        if stem_idx >= self.stems.len() {
-            let leaf_node = &self.leaves[stem_idx - self.stems.len()];
-
-            leaf_node
-                .content_points
-                .iter()
-                .enumerate()
-                .take(leaf_node.size)
-                .for_each(|(idx, entry)| {
-                    let distance = D::dist(query, entry);
-
-                    if distance < radius {
-                        matching_items.push(NearestNeighbour {
-                            distance,
-                            item: *unsafe { leaf_node.content_items.get_unchecked(idx) },
-                        });
-                    }
-                });
-
-            return;
-        }
-
-        let left_child_idx = stem_idx << 1;
-        self.prefetch_stems(left_child_idx);
-
-        let val = *unsafe { self.stems.get_unchecked(stem_idx) };
-        // let val = self.stems[stem_idx];
-
-        let mut rd = rd;
-        let old_off = off[split_dim];
-        let new_off = query[split_dim].saturating_dist(val);
-
-        let is_left_child = usize::from(*unsafe { query.get_unchecked(split_dim) } < val);
-        // let is_left_child = usize::from(query[split_dim] < val);
-
-        let closer_node_idx = left_child_idx + (1 - is_left_child);
-        let further_node_idx = left_child_idx + is_left_child;
-
-        let next_split_dim = (split_dim + 1).rem(K);
-
-        self.within_unsorted_recurse::<D>(
-            query,
-            radius,
-            closer_node_idx,
-            next_split_dim,
-            matching_items,
-            off,
-            rd,
-        );
-
-        rd = Axis::rd_update(rd, D::dist1(new_off, old_off));
-
-        if rd <= radius {
-            off[split_dim] = new_off;
-            self.within_unsorted_recurse::<D>(
-                query,
-                radius,
-                further_node_idx,
-                next_split_dim,
-                matching_items,
-                off,
-                rd,
-            );
-            off[split_dim] = old_off;
-        }
-    }
-}
-*/
 #[cfg(test)]
 mod tests {
     use crate::distance_metric::DistanceMetric;
@@ -203,22 +77,22 @@ mod tests {
     #[test]
     fn can_query_items_within_radius() {
         let content_to_add: [[AX; 4]; 16] = [
-            [0.9f32, 0.0f32, 0.9f32, 0.0f32],   // 1.34
-            [0.4f32, 0.5f32, 0.4f32, 0.51f32],  // 0.86
-            [0.12f32, 0.3f32, 0.12f32, 0.3f32], // 1.82
-            [0.7f32, 0.2f32, 0.7f32, 0.22f32],  // 0.86
-            [0.13f32, 0.4f32, 0.13f32, 0.4f32], // 1.56
-            [0.6f32, 0.3f32, 0.6f32, 0.33f32],  // 0.86
-            [0.2f32, 0.7f32, 0.2f32, 0.7f32],   // 1.46
-            [0.14f32, 0.5f32, 0.14f32, 0.5f32], // 1.38
-            [0.3f32, 0.6f32, 0.3f32, 0.6f32],   // 1.06
-            [0.10f32, 0.1f32, 0.10f32, 0.1f32], // 2.26
-            [0.16f32, 0.7f32, 0.16f32, 0.7f32], // 1.54
-            [0.1f32, 0.8f32, 0.1f32, 0.8f32],   // 1.86
-            [0.15f32, 0.6f32, 0.15f32, 0.6f32], // 1.36
-            [0.5f32, 0.4f32, 0.5f32, 0.44f32],  // 0.86
-            [0.8f32, 0.1f32, 0.8f32, 0.15f32],  // 0.86
-            [0.11f32, 0.2f32, 0.11f32, 0.2f32], // 2.04
+            [0.9f32, 0.0f32, 0.9f32, 0.0f32],
+            [0.4f32, 0.5f32, 0.4f32, 0.51f32],
+            [0.12f32, 0.3f32, 0.12f32, 0.3f32],
+            [0.7f32, 0.2f32, 0.7f32, 0.22f32],
+            [0.13f32, 0.4f32, 0.13f32, 0.4f32],
+            [0.6f32, 0.3f32, 0.6f32, 0.33f32],
+            [0.2f32, 0.7f32, 0.2f32, 0.7f32],
+            [0.14f32, 0.5f32, 0.14f32, 0.5f32],
+            [0.3f32, 0.6f32, 0.3f32, 0.6f32],
+            [0.10f32, 0.1f32, 0.10f32, 0.1f32],
+            [0.16f32, 0.7f32, 0.16f32, 0.7f32],
+            [0.1f32, 0.8f32, 0.1f32, 0.8f32],
+            [0.15f32, 0.6f32, 0.15f32, 0.6f32],
+            [0.5f32, 0.4f32, 0.5f32, 0.44f32],
+            [0.8f32, 0.1f32, 0.8f32, 0.15f32],
+            [0.11f32, 0.2f32, 0.11f32, 0.2f32],
         ];
 
         let tree: ImmutableKdTree<AX, u32, 4, 4> = ImmutableKdTree::new_from_slice(&content_to_add);
