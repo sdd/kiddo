50b94602425d3a418bcc5f48ff9d2a8f778afb70
Scott Donnelly <scott@donnel.ly>
2023-08-28 19:32:03 +0100

    chore: remove commented code and test comments

--- src/immutable/float/query/approx_nearest_one.rs
+++ src/immutable/float/query/approx_nearest_one.rs
@@ -64,81 +64,6 @@ impl<
     );
 }
 
-/*impl<A: Axis, T: Content, const K: usize, const B: usize> ImmutableKdTree<A, T, K, B> {
-    /// Queries the tree to find the approximate nearest element to `query`, using the specified
-    /// distance metric function.
-    ///
-    /// Faster than querying for nearest_one(point) due
-    /// to not recursing up the tree to find potentially closer points in other branches.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use kiddo::immutable::float::kdtree::ImmutableKdTree;
-    /// use kiddo::float::distance::SquaredEuclidean;
-    ///
-    /// let content: Vec<[f64; 3]> = vec!(
-    ///     [1.0, 2.0, 5.0],
-    ///     [2.0, 3.0, 6.0]
-    /// );
-    ///
-    /// let mut tree: ImmutableKdTree<f64, u32, 3, 32> = ImmutableKdTree::new_from_slice(&content);
-    ///
-    /// let nearest = tree.approx_nearest_one::<SquaredEuclidean>(&[1.0, 2.0, 5.1]);
-    ///
-    /// assert!((nearest.distance - 0.01f64).abs() < f64::EPSILON);
-    /// assert_eq!(nearest.item, 0);
-    /// ```
-    #[inline]
-    pub fn approx_nearest_one<F>(&self, query: &[A; K], distance_fn: &F) -> NearestNeighbour<A, T>
-    where
-        F: Fn(&[A; K], &[A; K]) -> A,
-    {
-        let mut split_dim = 0;
-        let mut stem_idx = 1;
-        let mut best_item = T::zero();
-        let mut best_dist = A::max_value();
-
-        let stem_len = self.stems.len();
-
-        while stem_idx < stem_len {
-            let left_child_idx = stem_idx << 1;
-            self.prefetch_stems(left_child_idx);
-
-            let val = *unsafe { self.stems.get_unchecked(stem_idx) };
-            let is_right_child = usize::from(*unsafe { query.get_unchecked(split_dim) } >= val);
-
-            stem_idx = left_child_idx + is_right_child;
-
-            split_dim += 1;
-            split_dim %= K;
-        }
-
-        let leaf_node = unsafe { self.leaves.get_unchecked(stem_idx - stem_len) };
-        // let leaf_node = &self.leaves[leaf_idx];
-
-        leaf_node
-            .content_points
-            .iter()
-            .enumerate()
-            .take(leaf_node.size)
-            .for_each(|(idx, entry)| {
-                let dist = distance_fn(query, entry);
-                if dist < best_dist {
-                    best_dist = dist;
-                    best_item = unsafe { *leaf_node.content_items.get_unchecked(idx) };
-                    // *best_item = leaf_node.content_items[idx]
-                }
-            });
-
-        NearestNeighbour {
-            distance: best_dist,
-            item: best_item,
-        }
-    }
-}
-*/
-
 #[cfg(test)]
 mod tests {
     use crate::float::distance::Manhattan;
@@ -150,22 +75,22 @@ mod tests {
     #[test]
     fn can_query_approx_nearest_one_item() {
         let content_to_add: [[AX; 4]; 16] = [
-            [0.9f32, 0.0f32, 0.9f32, 0.0f32],   // 1.34
-            [0.4f32, 0.5f32, 0.4f32, 0.51f32],  // 0.86
-            [0.12f32, 0.3f32, 0.12f32, 0.3f32], // 1.82
-            [0.7f32, 0.2f32, 0.7f32, 0.22f32],  // 0.86
-            [0.13f32, 0.4f32, 0.13f32, 0.4f32], // 1.56
-            [0.6f32, 0.3f32, 0.6f32, 0.33f32],  // 0.86
-            [0.2f32, 0.7f32, 0.2f32, 0.7f32],   // 1.46
-            [0.14f32, 0.5f32, 0.14f32, 0.5f32], // 1.38
-            [0.3f32, 0.6f32, 0.3f32, 0.6f32],   // 1.06
-            [0.10f32, 0.1f32, 0.10f32, 0.1f32], // 2.26
-            [0.16f32, 0.7f32, 0.16f32, 0.7f32], // 1.54
-            [0.1f32, 0.8f32, 0.1f32, 0.8f32],   // 1.86
-            [0.15f32, 0.6f32, 0.15f32, 0.6f32], // 1.36
-            [0.5f32, 0.4f32, 0.5f32, 0.44f32],  // 0.86
-            [0.8f32, 0.1f32, 0.8f32, 0.15f32],  // 0.86
-            [0.11f32, 0.2f32, 0.11f32, 0.2f32], // 2.04
+            [0.9f32, 0.0f32, 0.9f32, 0.0f32],
+            [0.4f32, 0.5f32, 0.4f32, 0.51f32],
+            [0.12f32, 0.3f32, 0.12f32, 0.3f32],
+            [0.7f32, 0.2f32, 0.7f32, 0.22f32],
+            [0.13f32, 0.4f32, 0.13f32, 0.4f32],
+            [0.6f32, 0.3f32, 0.6f32, 0.33f32],
+            [0.2f32, 0.7f32, 0.2f32, 0.7f32],
+            [0.14f32, 0.5f32, 0.14f32, 0.5f32],
+            [0.3f32, 0.6f32, 0.3f32, 0.6f32],
+            [0.10f32, 0.1f32, 0.10f32, 0.1f32],
+            [0.16f32, 0.7f32, 0.16f32, 0.7f32],
+            [0.1f32, 0.8f32, 0.1f32, 0.8f32],
+            [0.15f32, 0.6f32, 0.15f32, 0.6f32],
+            [0.5f32, 0.4f32, 0.5f32, 0.44f32],
+            [0.8f32, 0.1f32, 0.8f32, 0.15f32],
+            [0.11f32, 0.2f32, 0.11f32, 0.2f32],
         ];
 
         let tree: ImmutableKdTree<AX, u32, 4, 4> = ImmutableKdTree::new_from_slice(&content_to_add);
