c1885c57f291d70ddf28d35b9bb9902130688c57
Scott Donnelly <scott@donnel.ly>
2023-09-05 22:13:57 +0100

    feat(wip): simd leaf

---
+++ examples/simd_leaf.rs
@@ -0,0 +1,49 @@
+use std::error::Error;
+
+use kiddo::immutable::float::kdtree::{ImmutableKdTree};
+use kiddo::float_leaf_simd::leaf_node::LeafNode;
+
+use rand::{SeedableRng, Rng};
+use tracing_subscriber;
+
+const LEAF_COUNT: usize = 100;
+const QUERY_COUNT: usize = 1_000;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
+    let seed: u64 = 1;//31851;
+
+    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(seed);
+
+    let mut leaves: Vec<_> = Vec::with_capacity(LEAF_COUNT);
+
+    for _ in 0..LEAF_COUNT {
+        let simdleaf: LeafNode<f32, usize, 4, 32> = LeafNode {
+            content_points: rng.gen::<[[f32; 32]; 4]>(),
+            content_items: rng.gen::<[usize; 32]>(),
+            size: 32
+        };
+
+        leaves.push(simdleaf);
+    }
+
+    let mut query_points = Vec::with_capacity(QUERY_COUNT);
+    for _ in 0..QUERY_COUNT {
+        query_points.push(rng.gen::<[f32; 4]>());
+    }
+
+    for i in 0..QUERY_COUNT {
+        let mut best_dist = f32::INFINITY;
+        let mut best_item = usize::MAX;
+
+        let leaf_idx = rng.gen_range(0..LEAF_COUNT);
+        let leaf = &leaves[leaf_idx];
+
+        let query_point = &query_points[i];
+
+        let result = leaf.nearest_one(query_point, &mut best_dist, &mut best_item);
+        println!("result: {:?}", &result);
+    }
+    Ok(())
+}
---
+++ src/float_leaf_simd/leaf_node.rs
@@ -0,0 +1,108 @@
+use az::Az;
+use az::Cast;
+
+use super::fallback::get_best_from_dists_autovec;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+use super::f64_avx512::get_best_from_dists_f64_avx512;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+use super::f64_avx512::get_best_from_dists_f64_avx;
+
+use crate::{float::kdtree::Axis, types::Content};
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct LeafNode<A: Copy + Default, T: Copy + Default, const K: usize, const B: usize> {
+    pub content_points: [[A; B]; K],
+    pub content_items: [T; B],
+    pub size: usize,
+}
+
+pub trait BestFromDists<A, T, const B: usize> {
+    fn get_best_from_dists(acc: [A; B], best_dist: &mut A, best_item: &mut T);
+}
+
+impl<A, T, const K: usize, const B: usize> LeafNode<A, T, K, B>
+where
+    A: Axis + BestFromDists<A, T, B>,
+    T: Content,
+    usize: Cast<T>,
+{
+    fn new() -> Self {
+        LeafNode {
+            content_items: [T::zero(); B],
+            content_points: [[A::zero(); B]; K],
+            size: 0,
+        }
+    }
+
+    #[inline(never)]
+    pub fn nearest_one(self: &Self, query: &[A; K], best_dist: &mut A, best_item: &mut T) {
+
+        // AVX512: 4 loops of 32 iterations, each 4x unrolled, 5 instructions per pre-unrolled iteration
+        let mut acc = [A::zero(); B];
+        (0..K).step_by(1).for_each(|dim| {
+            let qd = [query[dim]; B];
+
+            (0..B).step_by(1).for_each(|idx| {
+                acc[idx] = acc[idx] + (self.content_points[dim][idx] - qd[idx]) * (self.content_points[dim][idx] - qd[idx]);
+            });
+        });
+
+        A::get_best_from_dists(acc, best_dist, best_item);
+
+        let (leaf_best_dist, leaf_best_item) = acc.iter().enumerate()
+            .fold((*best_dist, usize::MAX), |(bd, bi), (i, &d)| {
+            (bd.min(d), bi * usize::from(bd >= d) + i * usize::from(bd < d))
+        });
+
+        if leaf_best_dist < *best_dist {
+            *best_dist = leaf_best_dist;
+            *best_item = leaf_best_item.az::<T>();
+        }
+    }
+}
+
+impl<A: Axis, T: Content, const B: usize> BestFromDists<A, T, B> for f64
+    where usize: Cast<T>
+{
+    fn get_best_from_dists(acc: [A; B], best_dist: &mut A, best_item: &mut T) {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            if is_x86_feature_detected!("avx2") {
+                get_best_from_dists_f64_avx512(acc, best_dist, best_item)
+            } else if is_x86_feature_detected!("avx") {
+                get_best_from_dists_f64_avx(acc, best_dist, best_item)
+            } else {
+                get_best_from_dists_autovec(acc, best_dist, best_item)
+            }
+        }
+
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        {
+            get_best_from_dists_autovec(&acc, best_dist, best_item)
+        }
+    }
+}
+
+impl<A: Axis, T: Content, const B: usize> BestFromDists<A, T, B> for f32
+where usize: Cast<T>
+{
+    fn get_best_from_dists(acc: [A; B], best_dist: &mut A, best_item: &mut T) {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            if is_x86_feature_detected!("avx2") {
+                // TODO
+                unimplemented!()
+            } else if is_x86_feature_detected!("avx") {
+                // TODO
+                unimplemented!()
+            } else {
+                get_best_from_dists_autovec(acc, best_dist, best_item)
+            }
+        }
+
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        {
+            get_best_from_dists_autovec(&acc, best_dist, best_item)
+        }
+    }
+}
