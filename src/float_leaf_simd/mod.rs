279549ba1ff32da6bb5964bfef11388c78975e00
Scott Donnelly <scott@donnel.ly>
2023-10-10 23:24:21 +0100

    fix: disable AVX512 until I can test it on a machine with actual AVX512

--- src/float_leaf_simd/mod.rs
+++ src/float_leaf_simd/mod.rs
@@ -15,16 +15,18 @@ pub(crate) mod f32_avx2;
 ))]
 pub(crate) mod f64_avx2;
 
-#[cfg(all(
-    feature = "simd",
-    target_feature = "avx512",
-    any(target_arch = "x86", target_arch = "x86_64")
-))]
-pub(crate) mod f32_avx512;
+// TODO: test AVX512
 
-#[cfg(all(
-    feature = "simd",
-    target_feature = "avx512",
-    any(target_arch = "x86", target_arch = "x86_64")
-))]
-pub(crate) mod f64_avx512;
+// #[cfg(all(
+//     feature = "simd",
+//     target_feature = "avx512",
+//     any(target_arch = "x86", target_arch = "x86_64")
+// ))]
+// pub(crate) mod f32_avx512;
+
+// #[cfg(all(
+//     feature = "simd",
+//     target_feature = "avx512",
+//     any(target_arch = "x86", target_arch = "x86_64")
+// ))]
+// pub(crate) mod f64_avx512;
