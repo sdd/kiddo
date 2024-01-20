0a707fb7ad3f5891049a04d60bc7ea595dd9c429
Chris Barnes <chrislloydbarnes@gmail.com>
2024-01-20 14:57:14 +0000

    fix lints

--- src/iter.rs
+++ src/iter.rs
@@ -1,19 +1,21 @@
 use std::collections::VecDeque;
 
-use crate::fixed::kdtree as fixed;
-use crate::float::kdtree as float;
-use crate::immutable::float::kdtree as immut;
-use crate::types::{Content, Index};
+use crate::types::Content;
 
-pub(crate) type LeafData<A: Copy + Default, T: Content, const K: usize> = VecDeque<(T, [A; K])>;
+pub(crate) type LeafData<A, T, const K: usize> = VecDeque<(T, [A; K])>;
 
 pub(crate) trait IterableTreeData<A: Copy + Default, T: Content, const K: usize> {
     fn get_leaf_data(&self, idx: usize) -> Option<LeafData<A, T, K>>;
 }
 
 #[derive(Debug)]
-pub struct TreeIter<'a, A: Copy + Default, T: Content, const K: usize, X: IterableTreeData<A, T, K>>
-{
+pub(crate) struct TreeIter<
+    'a,
+    A: Copy + Default,
+    T: Content,
+    const K: usize,
+    X: IterableTreeData<A, T, K>,
+> {
     tree: &'a X,
     leaf_idx: usize,
     leaf_data: Option<LeafData<A, T, K>>,
@@ -23,7 +25,6 @@ impl<'a, A: Copy + Default, T: Content, const K: usize, X: IterableTreeData<A, T
     TreeIter<'a, A, T, K, X>
 {
     pub(crate) fn new(tree: &'a X) -> Self {
-        let leaf_data = tree.get_leaf_data(0);
         Self {
             tree,
             leaf_idx: 0,
@@ -38,13 +39,10 @@ impl<'a, A: Copy + Default, T: Content, const K: usize, X: IterableTreeData<A, T
     type Item = (T, [A; K]);
 
     fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            let mut data = self.leaf_data?;
-            if data.is_empty() {
-                self.leaf_data = self.tree.get_leaf_data(self.leaf_idx);
-                self.leaf_idx += 1;
-            }
-            return data.pop_front();
+        while self.leaf_data.as_ref()?.is_empty() {
+            self.leaf_data = self.tree.get_leaf_data(self.leaf_idx);
+            self.leaf_idx += 1;
         }
+        self.leaf_data.as_mut()?.pop_front()
     }
 }
