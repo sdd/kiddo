29ec09b8da91c928bf44b37bccc2f7d97fc143b7
Scott Donnelly <scott@donnel.ly>
2025-06-18 19:08:44 +0100

    test: fix rkyv_08 doctests

--- src/immutable/float/query/nearest_n_within.rs
+++ src/immutable/float/query/nearest_n_within.rs
@@ -93,10 +93,10 @@ where
         "use std::fs::File;
     use memmap::MmapOptions;
 
-    use kiddo::immutable::float::kdtree::AlignedArchivedImmutableKdTree;
+    use kiddo::immutable::float::kdtree::ArchivedImmutableKdTree;
 
-    let mmap = unsafe { MmapOptions::new().map(&File::open(\"./examples/immutable-doctest-tree.rkyv\").unwrap()).unwrap() };
-    let tree: AlignedArchivedImmutableKdTree<f64, u32, 3, 256> = AlignedArchivedImmutableKdTree::from_bytes(&mmap);"
+    let mmap = unsafe { MmapOptions::new().map(&File::open(\"./examples/immutable-doctest-tree_rkyv08.rkyv\").unwrap()).unwrap() };
+    let tree = unsafe { rkyv_08::access_unchecked::<ArchivedImmutableKdTree<f64, u32, 3, 256>>(&mmap) };"
     );
 }
 
