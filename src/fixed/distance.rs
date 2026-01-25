00b899f32e5dcdc51c939180cdea5068fceda042
Anirudh "Luca" Katoch <katoch.anirudh@gmail.com>
2026-01-25 23:11:39 +0530

    Update distance.rs doc for Manhatten distance

--- src/fixed/distance.rs
+++ src/fixed/distance.rs
@@ -8,10 +8,9 @@
 use crate::fixed::kdtree::Axis;
 use crate::traits::DistanceMetric;
 
-/// Returns the squared euclidean distance between two points. When you only
-/// need to compare distances, rather than having the exact distance between
-/// the points, this metric is beneficial because it avoids the expensive square
-/// root computation.
+/// Returns the Manhattan / "taxi cab" distance between two points.
+///
+/// Faster than squared Euclidean, and just as effective if not more so in higher-dimensional spaces
 ///
 /// # Examples
 ///
