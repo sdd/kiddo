bed9397ca078e0cc75ee1bff2420afd8ac347126
Scott Donnelly <scott@donnel.ly>
2026-04-15 07:47:11 +0100

    fix: prevent UB with compile-time check when bucket size = 1
    Fixes: https://github.com/sdd/kiddo/issues/295

--- src/fixed/kdtree.rs
+++ src/fixed/kdtree.rs
@@ -160,7 +160,16 @@ where
     T: Content,
     IDX: Index<T = IDX>,
 {
+    // Enforce bucket size of at least 2. B=1 can trigger UB in split fn
+    // https://github.com/sdd/kiddo/issues/295
+    const _VALID_B: () = {
+        assert!(B > 1, "Bucket size must be at least 2");
+    };
+
     pub(crate) fn new() -> Self {
+        #[allow(clippy::let_unit_value)]
+        let _ = Self::_VALID_B;
+
         Self {
             content_points: [[A::ZERO; K]; B],
             content_items: [T::zero(); B],
