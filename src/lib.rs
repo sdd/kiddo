b6065bc6c8e6469421c8ea6f92b6db739d73d452
Scott Donnelly <scott@donnel.ly>
2026-04-30 19:16:57 +0100

    feat: relabel from RC to alpha

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 #![warn(missing_docs)]
 #![warn(rustdoc::broken_intra_doc_links)]
 #![warn(rustdoc::private_intra_doc_links)]
-#![doc(html_root_url = "https://docs.rs/kiddo/6.0.0")]
+#![doc(html_root_url = "https://docs.rs/kiddo/6.0.0-alpha.1")]
 #![doc(issue_tracker_base_url = "https://github.com/sdd/kiddo/issues/")]
 #![allow(clippy::pointers_in_nomem_asm_block)]
 #![allow(clippy::too_many_arguments)]
